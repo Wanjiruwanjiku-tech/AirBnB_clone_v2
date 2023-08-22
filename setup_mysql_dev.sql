11477a569f32a76491983cf9e5a845d6db325954
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-08-22 07:05:37 +0300

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,10 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 --Grant Privileges to new user
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
-
--- Flush Privileges to ensure changes are made
-
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
