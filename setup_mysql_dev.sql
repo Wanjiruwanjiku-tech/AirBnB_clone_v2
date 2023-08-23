051504a9074555620bcc57cd03526f86b1e4f5af
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-08-23 05:44:38 +0300

    MySQL queries

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,5 +6,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 --Grant Privileges to new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- A script that prepares a MySQL server for 
+-- the project
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create New User
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant Privileges
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
