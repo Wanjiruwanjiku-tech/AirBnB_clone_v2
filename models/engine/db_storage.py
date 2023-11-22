f5968a2c81aca5f49b034a37c6330a3e2fdc2f55
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-11-22 12:50:18 +0300

    DBStorage updates

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@ import os
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -14,18 +14,20 @@ from models.review import Review
 
 class DBStorage():
     """ DataBase for hbnb """
-
+    
+    # Private class attrs engine and session set to none
     __engine = None
     __session = None
 
     def __init__(self):
         """ Initialize the class """
+        # Create the engine
         user = os.getenv("HBNB_MYSQL_USER")
         passwd = os.getenv("HBNB_MYSQL_PWD")
         host = os.getenv("HBNB_MYSQL_HOST")
         db = os.getenv("HBNB_MYSQL_DB")
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, passwd, host, db))
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, passwd, host, db), pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
     
@@ -39,7 +41,7 @@ class DBStorage():
                 objects[key] = obj
         else:
             for sub_cls in Base.__subclasses__():
-                for obj in self.__session.query(cls).all():
+                for obj in self.__session.query(sub_cls).all():
                     key = "{}.{}".format(type(obj).__name__, obj.id)
                     objects[key] = obj
         return objects
@@ -60,7 +62,7 @@ class DBStorage():
     def reload(self):
         """ Creates all tables """
         Base.metadata.create_all(self.__engine)
-        Mysession = sessionmaker(bind=self.__engine, expire_on_commit=false)
+        Mysession = sessionmaker(bind=self.__engine, expire_on_commit=False)
 
         Session = scoped_session(Mysession)
         self.__session = Session()
